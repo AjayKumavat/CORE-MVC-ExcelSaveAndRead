774a84d671864f56c6beabd7da5052c7dc414261
Ajay Kumawat <akmvt0@gmail.com>
2021-08-28 18:49:31 +0530

    Add project files.

---
+++ CORE-MVC-ExcelSaveAndRead/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CORE_MVC_ExcelSaveAndRead.Models
+{
+    public class Student
+    {
+        public string Name { get; set; } = "";
+        public string Roll { get; set; } = "";
+        public string Email { get; set; } = "";
+    }
+}
